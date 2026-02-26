2ea60e1446d516699a3882f62fd3f435baf1fa35
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-02-26 22:09:37 +0530

    logic shifting

--- src/data_processing.py
+++ src/data_processing.py
@@ -13,23 +13,12 @@ Spark = SparkSession.builder.appName("data_processing").getOrCreate()
 
 df_raw = Spark.read.option("header", True).option("inferSchema", True).csv("sales_data_dirty.csv")
 
-# DATA VALIDATION
-df_bad = df_raw.filter(
-    (col("price") <= 0) |
-    (col("quantity") <= 0) |
-    (col("order_date") > current_date()) |
-    (col("price").isNull()) |
-    (col("quantity").isNull())
-)
 
-df_good = df_raw.subtract(df_bad).dropDuplicates(["order_id"]).dropna(subset=["order_id","price","quantity","order_date","region"]).cache()
-
-df_good.show()
 
 #DATA CLEANING
-df_cleaned = df_good.withColumn("order_date",to_date(col("order_date"),"yyyy-MM-DD")) \
-    .withColumn("price",df_good["price"].cast(DoubleType())) \
-    .withColumn("quantity",df_good["quantity"].cast(IntegerType())) \
+df_cleaned = df_raw.withColumn("order_date",to_date(col("order_date"),"yyyy-MM-dd")) \
+    .withColumn("price",df_raw["price"].cast(DoubleType())) \
+    .withColumn("quantity",df_raw["quantity"].cast(IntegerType())) \
     .withColumn("product_name",trim(col("product_name"))) \
     .withColumn("category",trim(col("category"))).cache()
 
@@ -39,8 +28,21 @@ print("the cleaned data : ")
 
 df_cleaned.show()
 
+# DATA VALIDATION
+df_bad = df_cleaned.filter(
+    (col("price") <= 0) |
+    (col("quantity") <= 0) |
+    (col("order_date") > current_date()) |
+    (col("price").isNull()) |
+    (col("quantity").isNull())
+)
+
+df_good = df_cleaned.subtract(df_bad).dropDuplicates(["order_id"]).dropna(subset=["order_id","price","quantity","order_date","region"]).cache()
+
+df_good.show()
+
 #DATA TRANSFORMATION
-df_transformed = df_cleaned.withColumn("total", col("price")* col("quantity")) \
+df_transformed = df_good.withColumn("total", col("price")* col("quantity")) \
     .withColumn("year",year(col("order_date"))) \
     .withColumn("month",month(col("order_date"))) \
     .withColumn("day",day(col("order_date"))) \
