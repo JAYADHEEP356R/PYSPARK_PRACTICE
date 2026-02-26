7cbdebf60d2c5c85e1c6d0d5e88a15cfbbb185d0
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-02-26 22:48:33 +0530

    data transformation phase-1

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,31 +1,26 @@
 from pyspark.sql.types import *
-
 from OS.Initialize import doinit
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-
-doinit()
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, current_date
+doinit()
 
 Spark = SparkSession.builder.appName("data_processing").getOrCreate()
-
 df_raw = Spark.read.option("header", True).option("inferSchema", True).csv("sales_data_dirty.csv")
-
-
 
 #DATA CLEANING
 df_cleaned = df_raw.withColumn("order_date",to_date(col("order_date"),"yyyy-MM-dd")) \
     .withColumn("price",df_raw["price"].cast(DoubleType())) \
     .withColumn("quantity",df_raw["quantity"].cast(IntegerType())) \
     .withColumn("product_name",trim(col("product_name"))) \
-    .withColumn("category",trim(col("category"))).cache()
-
+    .withColumn("category",trim(col("category"))).cache() \
+    .withColumn("payment_method",when(col("payment_method").isNull(),"UNKNOWN").otherwise(col("payment_method"))) \
+    .withColumn("region",when(col("region").isNull(),"UNKNOWN").otherwise(col("region"))) \
+    .cache()
 
 
 print("the cleaned data : ")
-
 df_cleaned.show()
 
 # DATA VALIDATION
@@ -41,11 +36,12 @@ df_good = df_cleaned.subtract(df_bad).dropDuplicates(["order_id"]).dropna(subset
 
 df_good.show()
 
-#DATA TRANSFORMATION
-df_transformed = df_good.withColumn("total", col("price")* col("quantity")) \
+#DATA TRANSFORMATION - PHASE 1
+df_transformed = df_good.withColumn("total_amount", col("price")* col("quantity")) \
     .withColumn("year",year(col("order_date"))) \
     .withColumn("month",month(col("order_date"))) \
     .withColumn("day",day(col("order_date"))) \
+    .withColumn("year_month",concat_ws("-",col("year"),col("month"))) \
     .cache()
 
 df_transformed.show()
